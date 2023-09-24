0c587254e9f0d82a8b67f205b4de48cff9d512f3
UnderSaga <undersaga@mail.ru>
2023-09-24 12:00:38 +0300

    Сделал калкалкалкалк

---
+++ src/main/java/Main.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Double firstNum;
+        Double secondNum;
+        String symbol;
+        double res;
+        double res1;
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.print("Введите первое число: ");
+            firstNum = in.nextDouble();
+            System.out.print("Введите операцию: ");
+            symbol = in.next();
+            System.out.print("Введите второе число: ");
+            secondNum = in.nextDouble();
+            switch (symbol) {
+                case ("/"):
+                    res = firstNum / secondNum;
+                    res1 = Math.floor(res);
+                    if (res - res1 == 0.0){
+                        long result = Math.round(res);
+                        System.out.print("Результат: " + result);
+                        break;
+                    }
+                    System.out.print("Результат: " + res);
+                    break;
+                case ("*"):
+                    res = firstNum * secondNum;
+                    res1 = Math.floor(res);
+                    if (res - res1 == 0.0){
+                        long result = Math.round(res);
+                        System.out.print("Результат: " + result);
+                        break;
+                    }
+                    System.out.print("Результат: " + res);
+                    break;
+                case ("+"):
+                    res = firstNum + secondNum;
+                    res1 = Math.floor(res);
+                    if (res - res1 == 0.0){
+                        long result = Math.round(res);
+                        System.out.print("Результат: " + result);
+                        break;
+                    }
+                    System.out.print("Результат: " + res);
+                    break;
+                case ("-"):
+                    res = firstNum - secondNum;
+                    res1 = Math.floor(res);
+                    if (res - res1 == 0.0){
+                        long result = Math.round(res);
+                        System.out.print("Результат: " + result);
+                        break;
+                    }
+                    System.out.print("Результат: " + res);
+                    break;
+                default:
+                    System.out.println("Введеная неверная операция!");
+            }
+        } catch (Exception e) {
+            System.out.println("Неверный формат числа");
+        }
+    }
+}
